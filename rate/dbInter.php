6f1725c5d7f4ba0ca6667e0f164c4c4ad7387a7b
gatesyp <ig11@zips.uakron.edu>
2016-01-24 18:45:34 -0500

    updated ajax/jquery with example

--- rate/dbInter.php
+++ rate/dbInter.php
@@ -1,8 +1,33 @@
 <?php
 // error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 require 'dbConnect.php';
+if (is_ajax()) {
+  if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
+    $action = $_POST["action"];
+    switch($action) { //Switch case for value of action
+      case "test": test_function(); break;
+    }
+  }
+}
 
-function giveResponse(){
+//Function to check if the request is an AJAX request
+function is_ajax() {
+  return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
+
+function test_function(){
+  $return = $_POST;
+
+  //Do what you need to do with the info. The following are some examples.
+  if ($return["favorite_beverage"] == ""){
+    $return["favorite_beverage"] = "Coke";
+  }
+  $return["favorite_restaurant"] = "McDonald's";
+
+  $return["json"] = json_encode($return);
+  echo json_encode($return);
+}
+/*function giveResponse(){
   global $conn;
   $url;
   $sql = "SELECT url FROM pictures WHERE id = ". rand(1, 2);
@@ -24,3 +49,4 @@ else{
   $url = giveResponse();
 }
 mysqli_close($con);
+*/
--- rate/index.php
+++ rate/index.php
@@ -4,61 +4,43 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-  <script>
-  function getID(){
-    // facebook operation to get their profile id
-    // return id;
-  }
-  function firstTime(){
-    xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function() {
-      if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-        return xmlhttp.responseText;
-//        document.getElementById("txtHint").innerHTML= xmlhttp.responseText;
-}
-    }
-    xmlhttp.open("GET","dbInter.php?q=9",true);
-    xmlhttp.send();
-  }
-  function sendData(str) {
-    if (str=="") {
-      document.getElementById("txtHint").innerHTML="";
-      return;
-    }
-    if (window.XMLHttpRequest) {
-      // code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-    }
-    xmlhttp.onreadystatechange=function() {
-      if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-        // get the response which will just be a plain URL, construct the img tag for it and display it to "txtHint"
-        // that saved image url will go back to GET parameters under key "url" for DB use
-        // document.getElementById("txtHint").innerHTML=
-        dataHelper(xmlhttp.responseText);
-      }
-    }
-    xmlhttp.open("GET","dbInter.php?q="+str,true);
-    xmlhttp.send();
-  }
-
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript">
+  $("document").ready(function(){
+    $(".js-ajax-php-json").submit(function(){
+      var data = {
+        "action": "test"
+      };
+      data = $(this).serialize() + "&" + $.param(data);
+      $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "dbInter.php", //Relative or absolute path to response.php file
+        data: data,
+        success: function(data) {
+          $(".the-return").html(
+            "Favorite beverage: " + data["favorite_beverage"] + "<br />Favorite restaurant: " + data["favorite_restaurant"] + "<br />Gender: " + data["gender"] + "<br />JSON: " + data["json"]
+          );
+          alert("Form submitted successfully.\nReturned json: " + data["json"]);
+        }
+      });
+      return false;
+    });
+  });
   </script>
 </head>
-<body onload = "firstTime()">
-  <div class="container">
-
-    <br>
-    <div id="txtHint">
-      <img src= "firstTime()">
-    </div>
+<body>
+  <form action="return.php" class="js-ajax-php-json" method="post" accept-charset="utf-8">
+    <input type="text" name="favorite_beverage" value="" placeholder="Favorite restaurant" />
+    <input type="text" name="favorite_restaurant" value="" placeholder="Favorite beverage" />
+    <select name="gender">
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+    </select>
+    <input type="submit" name="submit" value="Submit form" />
+  </form>
+  <div class="the-return">
+    [HTML is replaced when successful.]
   </div>
-  <p> LIKE AND DISLIKE BUTTON WILL GO HERE REPLACING THE FORM
-    <form>
-      <select name="users" onchange="sendData(this.value)">
-        <option value="">Select a person:</option>
-        <option value="0">Like</option>
-        <option value="1">Dislike</option>
-      </select>
-    </form>
-  </body>
-  </html>
+</body>
+</html>
